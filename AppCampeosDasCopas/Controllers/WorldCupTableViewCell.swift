3ae60ab975f6e67db4a635fb8b437d44a5a80fc6
Renato Dias <renatocardosodias@gmail.com>
2022-09-08 09:25:10 -0300

    Criado UITableViewCell nomeada de WorldCupTableViewCell

---
+++ AppCampeosDasCopas/Controllers/WorldCupTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  WorldCupTableViewCell.swift
+//  AppCampeosDasCopas
+//
+//  Created by Renato on 08/09/22.
+//
+
+import UIKit
+
+class WorldCupTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
